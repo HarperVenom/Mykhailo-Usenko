39c6886e5ed50a8252cefc58244455cf1c7a915f
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-06-15 13:12:26 +0300

    completed

--- scripts/page-animations.js
+++ scripts/page-animations.js
@@ -1,13 +1,12 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  }
-  // { root: null, rootMargin: "0px 0px 50% 0px", threshold: 0 }
-);
+window.scrollTo(0, 0);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe(element));
